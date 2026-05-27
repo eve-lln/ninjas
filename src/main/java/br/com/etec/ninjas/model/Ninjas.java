19849bdae5fa3ec0be492908480b92df66124aed
evelli <evellinventura2019@gmail.com>
2026-05-26 22:45:40 -0300

    atualizacao

--- src/main/java/br/com/etec/ninjas/model/Ninjas.java
+++ src/main/java/br/com/etec/ninjas/model/Ninjas.java
@@ -6,16 +6,18 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
 
+@Data
 @Entity
 @Table (name = "ninja")//Nome diferente do nome da nossa classe 
 
 //Construtores vazios 
 public class Ninjas {
-    @Id//sempre colocar ela a cima da chave que vai ser primaria 
-    @GeneratedValue(strategy =  GenerationType.AUTO)//vamos gerar uma sequencia de dados, de maneira automatica que vem do bd, inicia no 1 e ir dando seuquencia 
+    @Id//sempre colocar ela acima da chave que vai ser primaria 
+    @GeneratedValue(strategy =  GenerationType.AUTO)//vamos gerar uma sequencia de dados, de maneira automatica que vem do bd, inicia no 1 e ir dando sequencia 
     private Long id_ninja;
-                                //nullable = nao pode ser nulo
+    //nullable = nao pode ser nulo
     @Column(name = "nome_ninja", nullable = false)//fica em cima, é a anotacao e quem vai receber a regra
     private String nome;
     @Column(name = "cpf_ninja", nullable = false, unique = true)
@@ -25,36 +27,5 @@ public class Ninjas {
     public Ninjas() {
     }
 
-    //Construtores cheios 
-    public Ninjas(String nome, String cpf, String email) {
-        this.nome = nome;
-        this.cpf = cpf;
-        this.email = email;
-    }
-
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
     
 }
